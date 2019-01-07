5542108a0ed52138b97ea79b3957390acd7ca94f
ABU-MUHAMMED <ABU-MUHAMMED@DESKTOP-36PINDJ>
2019-01-07 21:50:04 +0300

    kac kelimeden olustugu

---
+++ ConsoleApplication5/ConsoleApplication5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ifade;
+            ifade = Console.ReadLine();
+            string[] sonuc = ifade.Split(' ');
+            Console.WriteLine(" girilen ifade{0}olusan", sonuc.Length);
+            foreach(string kelime in sonuc)
+            {
+                Console.WriteLine(kelime);
+
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
