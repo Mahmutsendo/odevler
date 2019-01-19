eb82847f278387d5007effdfbd6f1ad2ba2a1afa
ABU-MUHAMMED <ABU-MUHAMMED@DESKTOP-36PINDJ>
2019-01-19 16:59:02 +0300

    kare kok

---
+++ ConsoleApplication10/ConsoleApplication10/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication10
+{
+    class Program
+    {
+        public static double foktoryel(int n)
+        {
+            if (n == 0)
+                return 1;
+            else
+                return n * foktoryel(n - 1);
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            double f = foktoryel(n);
+            Console.WriteLine(f);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ConsoleApplication11/ConsoleApplication11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 500; i++)
+            {
+                int kok = Convert.ToInt32(Math.Sqrt(i));
+                if (kok * kok == i)
+                    Console.WriteLine(i);
+            }
+                Console.ReadKey();
+            
+        }
+    }
+}
