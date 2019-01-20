0110b846098452b5e6a9479a8b057c7b5f987a1d
ABU-MUHAMMED <ABU-MUHAMMED@DESKTOP-36PINDJ>
2019-01-20 22:44:19 +0300

    153 sayıların carpma,toplamı

---
+++ ConsoleApplication12/ConsoleApplication12/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tavsan = 1520;
+            int kuslar = 3256;
+            int ay = 0;
+            while (tavsan < kuslar)
+            {
+                kuslar = kuslar + kuslar * 37 / 1000;
+                tavsan = tavsan + tavsan * 13 / 1000;
+                ay++;
+
+
+            }
+            Console.WriteLine(ay);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ConsoleApplication14/ConsoleApplication14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           for(int i=0; i<=153;i++)
+            {
+                for(int j=0; j<153; j++)
+                {
+                    if(i*6+j*9==153)
+                    {
+                        Console.WriteLine("{0}*6+{1}*9=153", i, j);
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
